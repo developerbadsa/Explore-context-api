1d78f29cecfb340c828fcfd530784952e02ec3f2
Rahim_Badsa <developerbadsa@gmail.com>
2023-10-01 07:17:22 +0100

    added nav bar and making router access

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import UserProvider from './Components/UserProvider.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import MainLayOut from './Layout/MainLayOut.jsx'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayOut></MainLayOut>,
+    children: [
+      {
+        path: '/',
+        element: <Login></Login>
+      },
+      {
+        path: '/',
+        element: <Register></Register>
+      }
+    ]
+  }
+])
+
+
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 
   <React.StrictMode>
     <UserProvider>
-      <App></App>
+      <RouterProvider router={router}></RouterProvider>
     </UserProvider>
   </React.StrictMode>,
 )
